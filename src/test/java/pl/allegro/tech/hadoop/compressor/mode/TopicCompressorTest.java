20bf8ad08d0bcf6b469d589483998ccc08cac887
Piotr Wikiel <piotr.wikiel@allegrogroup.com>
2017-01-19 16:49:06 +0100

    fixed unit tests

--- src/test/java/pl/allegro/tech/hadoop/compressor/mode/TopicCompressorTest.java
+++ src/test/java/pl/allegro/tech/hadoop/compressor/mode/TopicCompressorTest.java
@@ -1,8 +1,12 @@
 package pl.allegro.tech.hadoop.compressor.mode;
 
+import org.apache.hadoop.fs.BatchedRemoteIterator;
+import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +23,7 @@ import java.util.Calendar;
 import static java.util.Calendar.DAY_OF_MONTH;
 import static java.util.Calendar.MONTH;
 import static java.util.Calendar.YEAR;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyZeroInteractions;
@@ -26,7 +31,8 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TopicCompressorTest {
-    private static final FileStatus[] EMPTY_STATUSES = {};
+
+    private static final RemoteIterator<LocatedFileStatus> EMPTY_STATUSES = new FakeRemoteIterator<>(new LocatedFileStatus[] {});
 
     @Mock
     private FileSystem fileSystem;
@@ -41,26 +47,27 @@ public class TopicCompressorTest {
 
     @Before
     public void setUp() {
-        when(options.getTopicModePatterns()).thenReturn(Arrays.asList("hourly/*/*/*/*", "daily/*/*/*"));
+        when(options.getTopicModePatterns()).thenReturn(Arrays.asList("hourly", "daily"));
         topicCompressor = new TopicCompressor(fileSystem, unitCompressor, new TopicDateFilter(1), options);
     }
 
     @Test
     public void shouldCompressForAllHours() throws Exception {
         // given
-        Path hour1 = new Path("test_hour_path_1");
-        Path hour2 = new Path("test_hour_path_2");
-        when(fileSystem.globStatus(new Path("topic_dir/hourly/*/*/*/*"))).thenReturn(new FileStatus[]{
+        Path hour1 = new Path("/topic_dir/hourly/2016/01/01/10/file1.deflate");
+        Path hour2 = new Path("/topic_dir/hourly/2016/01/02/10/file1.deflate");
+        when(fileSystem.exists(any(Path.class))).thenReturn(true);
+        when(fileSystem.listFiles(new Path("/topic_dir/hourly"), true)).thenReturn(new FakeRemoteIterator<>(new LocatedFileStatus[]{
                 fileStatusForPath(hour1), fileStatusForPath(hour2)
-        });
-        when(fileSystem.globStatus(new Path("topic_dir/daily/*/*/*"))).thenReturn(EMPTY_STATUSES);
+        }));
+        when(fileSystem.listFiles(new Path("/topic_dir/daily"), true)).thenReturn(EMPTY_STATUSES);
 
         // when
-        topicCompressor.compress("topic_dir");
+        topicCompressor.compress("/topic_dir");
 
         // then
-        verify(unitCompressor).compress(hour1);
-        verify(unitCompressor).compress(hour2);
+        verify(unitCompressor).compress(hour1.getParent());
+        verify(unitCompressor).compress(hour2.getParent());
         verifyNoMoreInteractions(unitCompressor);
 
     }
@@ -68,19 +75,20 @@ public class TopicCompressorTest {
     @Test
     public void shouldCompressForAllHDays() throws Exception {
         // given
-        Path day1 = new Path("test_day_path_1");
-        Path day2 = new Path("test_day_path_2");
-        when(fileSystem.globStatus(new Path("topic_dir/hourly/*/*/*/*"))).thenReturn(EMPTY_STATUSES);
-        when(fileSystem.globStatus(new Path("topic_dir/daily/*/*/*"))).thenReturn(new FileStatus[] {
+        Path day1 = new Path("/topic_dir/daily/2016/01/01/file1.deflate");
+        Path day2 = new Path("/topic_dir/daily/2016/01/02/file1.deflate");
+        when(fileSystem.listFiles(new Path("/topic_dir/hourly"), true)).thenReturn(EMPTY_STATUSES);
+        when(fileSystem.exists(any(Path.class))).thenReturn(true);
+        when(fileSystem.listFiles(new Path("/topic_dir/daily"), true)).thenReturn(new FakeRemoteIterator<>(new LocatedFileStatus[] {
                 fileStatusForPath(day1), fileStatusForPath(day2)
-        });
+        }));
 
         // when
-        topicCompressor.compress("topic_dir");
+        topicCompressor.compress("/topic_dir");
 
         // then
-        verify(unitCompressor).compress(day1);
-        verify(unitCompressor).compress(day2);
+        verify(unitCompressor).compress(day1.getParent());
+        verify(unitCompressor).compress(day2.getParent());
         verifyNoMoreInteractions(unitCompressor);
 
     }
@@ -90,10 +98,11 @@ public class TopicCompressorTest {
         // given
         Calendar cal = Calendar.getInstance();
         Path todayPath = new Path(String.format("test/%4d/%02d/%02d/today", cal.get(YEAR), cal.get(MONTH)+1, cal.get(DAY_OF_MONTH)));
-        when(fileSystem.globStatus(new Path("topic_dir/hourly/*/*/*/*"))).thenReturn(new FileStatus[] {
+        when(fileSystem.listFiles(new Path("topic_dir/hourly"), true)).thenReturn(new FakeRemoteIterator<>(new LocatedFileStatus[] {
                 fileStatusForPath(todayPath)
-        });
-        when(fileSystem.globStatus(new Path("topic_dir/daily/*/*/*"))).thenReturn(EMPTY_STATUSES);
+        }));
+        when(fileSystem.exists(any(Path.class))).thenReturn(true);
+        when(fileSystem.listFiles(new Path("topic_dir/daily"), true)).thenReturn(EMPTY_STATUSES);
 
         // when
         topicCompressor.compress("topic_dir");
@@ -102,33 +111,30 @@ public class TopicCompressorTest {
         verifyZeroInteractions(unitCompressor);
     }
 
-    @Test
-    public void shouldNotAvoidCompressingFilesWithStrangePath() throws IOException {
-        // given
-        Calendar cal = Calendar.getInstance();
-        String todayDir = String.format("%4d/%02d/%02d", cal.get(YEAR), cal.get(MONTH)+1, cal.get(DAY_OF_MONTH));
-        cal.add(DAY_OF_MONTH, -1);
-        String yesterdayDir = String.format("%4d/%02d/%02d", cal.get(YEAR), cal.get(MONTH)+1, cal.get(DAY_OF_MONTH));
-        String base = String.format("/camus/%s/mytopic", todayDir);
-        when(fileSystem.globStatus(new Path(String.format("%s/hourly/*/*/*/*", base)))).thenReturn(EMPTY_STATUSES);
-        when(fileSystem.globStatus(new Path(String.format("%s/daily/*/*/*", base)))).thenReturn(new FileStatus[]{
-                fileStatusForPath(String.format("%s/%s", base, todayDir)),
-                fileStatusForPath(String.format("%s/%s", base, yesterdayDir))
-        });
-
-        // when
-        topicCompressor.compress(base);
-
-        // then
-        verify(unitCompressor).compress(new Path(String.format("%s/%s", base, yesterdayDir)));
-        verifyNoMoreInteractions(unitCompressor);
+    private static LocatedFileStatus fileStatusForPath(Path path) throws IOException {
+        final FileStatus stat = new FileStatus(10, true, 3, 1024, 100, path);
+        return new LocatedFileStatus(stat, new BlockLocation[] {});
     }
 
-    private static FileStatus fileStatusForPath(Path path) {
-        return new FileStatus(10, true, 3, 1024, 100, path);
-    }
-
-    private static FileStatus fileStatusForPath(String path) {
+    private static LocatedFileStatus fileStatusForPath(String path) throws IOException {
         return fileStatusForPath(new Path(path));
+    }
+
+    private static class FakeRemoteIterator<E> implements RemoteIterator<E> {
+        private E[] elements;
+        private int count;
+
+        FakeRemoteIterator(E[] elements) {
+            this.elements = elements;
+            this.count = 0;
+        }
+
+        public boolean hasNext() throws IOException {
+            return this.count < this.elements.length;
+        }
+
+        public E next() throws IOException {
+            return this.hasNext() ? this.elements[this.count++] : null;
+        }
     }
 }
