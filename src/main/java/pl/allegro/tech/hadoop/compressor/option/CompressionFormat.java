2c1d5b63c2fca07865648d0c8480b2ffe989e504
Michal Ferlinski <michal.ferlinski@allegro.pl>
2019-07-15 11:43:49 +0200

    Gzip compression added

--- src/main/java/pl/allegro/tech/hadoop/compressor/option/CompressionFormat.java
+++ src/main/java/pl/allegro/tech/hadoop/compressor/option/CompressionFormat.java
@@ -1,7 +1,7 @@
 package pl.allegro.tech.hadoop.compressor.option;
 
 public enum CompressionFormat {
-    SNAPPY, DEFLATE, LZO, NONE;
+    SNAPPY, GZIP, DEFLATE, LZO, NONE;
 
     public static CompressionFormat fromString(String format) {
         return CompressionFormat.valueOf(format.toUpperCase());
